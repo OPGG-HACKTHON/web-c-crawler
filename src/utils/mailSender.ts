bcce139855e831826c00aa300290281761a68153
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-08-04 16:33:44 +0900

    Feat : :zap:️ 팀원 메일 추가
    백엔드 팀원 두명의 메일을 추가하였다.

--- src/utils/mailSender.ts
+++ src/utils/mailSender.ts
@@ -4,7 +4,7 @@ const sendMail = async (text: string) => {
   const title = text === 'UPDATE SUCCESS' ? text : 'Update ERROR';
   await transporter.sendMail({
     from: `"WDMA Team" <kanghg1116>`,
-    to: 'kanghg1116@gmail.com',
+    to: 'kanghg1116@gmail.com, dosemfb0920@gmail.com, dnstlr2933@naver.com',
     subject: title,
     text,
   });
