bb8e19ed8ad37b165ce30032c06b530cedf12815
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-10-30 20:07:41 +0900

    https

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import fs from 'fs'
 import Initializer from './initializer/index';
+const https = require('https');
 require('dotenv').config();
+
+const options = { key: fs.readFileSync('/home/ubuntu/app/private.key'), cert: fs.readFileSync('/home/ubuntu/app/certificate.crt') };
 
 const startServer = () => {
   const app: express.Application = express();
   const { SERVER_PORT } = process.env;
   const initializer: Initializer = new Initializer(app);
   initializer.init();
-  app.listen(SERVER_PORT, () => console.log('SUCCESS'));
+  https.createServer(options, app).listen(SERVER_PORT);
 };
 
 startServer();
