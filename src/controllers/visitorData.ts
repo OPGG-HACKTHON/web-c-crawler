371d83044881dccea1afdf5a860baed3324f4f5a
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-10-30 20:37:54 +0900

    데이터 추가

--- src/controllers/visitorData.ts
+++ src/controllers/visitorData.ts
@@ -2,12 +2,21 @@ import { Request, Response } from 'express';
 import fs from 'fs';
 import errorHandler from '../utils/errorHandler';
 
+console.log(new Date())
 class VisitorController {
     public visit (req: Request, res: Response) {
         try {
             const data = JSON.parse(fs.readFileSync('static/visitor.json').toString());
             data.totalVisitor = String(parseInt(data.totalVisitor) + 1);
-            data.visitorInfo.push({date: new Date()})
+            const currentDate = String(new Date());
+            const currentDay = currentDate.split('T')[0]
+            const ip = req.headers['x-forwarded-for'] ?? 'unknown';
+            const ipStr = String(ip)
+            if(!data.visitorInfo[ipStr]) data.visitorInfo[ipStr] = '0';
+            data.visitorInfo[ipStr] = String(parseInt(data.totalVisitor) + 1);
+            
+            if(!data.DAU[currentDay]) data.DAU[currentDay] = '0';
+            data.DAU[currentDay] = String(parseInt(data.DAU[currentDay]) + 1);
             fs.writeFile('static/visitor.json', JSON.stringify(data), () => {});
             res.send()
         }catch(err){
