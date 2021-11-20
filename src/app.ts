57d702badde78c20cd6bcfbdf32ba62a429097d5
Kang-Hyun-Gu <kanghg1116@naver.com>
2021-11-20 16:24:17 +0900

    https 삭제 및 pulic ip로 변경

--- src/app.ts
+++ src/app.ts
@@ -6,24 +6,24 @@ require('dotenv').config();
 
 // const options = { key: fs.readFileSync('/home/ec2-user/swoomi/backend_swoomi_me.p12') };
 
-const options = {
-  url: 'https://backend.swoomi.me/',
-  headers: {
-      "content-type": "application/json",
-  },
-  agentOptions: {
-      pfx: fs.readFileSync('/home/ec2-user/swoomi/backend_swoomi_me.p12'),
-      passphrase: ''
-  }
-};
+// const options = {
+//   url: 'https://backend.swoomi.me/',
+//   headers: {
+//       "content-type": "application/json",
+//   },
+//   agentOptions: {
+//       pfx: fs.readFileSync('/home/ec2-user/swoomi/backend_swoomi_me.p12'),
+//       passphrase: ''
+//   }
+// };
 
 const startServer = () => {
   const app: express.Application = express();
   const { SERVER_PORT } = process.env;
   const initializer: Initializer = new Initializer(app);
   initializer.init();
-  https.createServer(options, app).listen(SERVER_PORT);
-  // app.listen(SERVER_PORT)
+  // https.createServer(options, app).listen(SERVER_PORT);
+  app.listen(SERVER_PORT)
 };
 
 startServer();
